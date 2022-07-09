7b067d6dc007fd6637c7df7ad3b24f4e6d674cbe
callisto1337 <hazardous333@gmail.com>
2022-07-09 16:21:04 +0300

    test

--- sw.js
+++ sw.js
@@ -1,27 +1,2 @@
-// Choose a cache name
-const cacheName = 'cache-v1';
-// List the files to precache
-const precacheResources = ['/', '/index.html', '/index.js'];
-
-// When the service worker is installing, open the cache and add the precache resources to it
-self.addEventListener('install', (event) => {
-    console.log('Service worker install event!');
-    event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(precacheResources)));
-});
-
-self.addEventListener('activate', (event) => {
-    console.log('Service worker activate event!');
-});
-
-// When there's an incoming fetch request, try and respond with a precached resource, otherwise fall back to the network
-self.addEventListener('fetch', (event) => {
-    console.log('Fetch intercepted for:', event.request.url);
-    event.respondWith(
-        caches.match(event.request).then((cachedResponse) => {
-            if (cachedResponse) {
-                return cachedResponse;
-            }
-            return fetch(event.request);
-        }),
-    );
-});
+(()=>{let e=[],n="";e=["/index.html","/manifest.webmanifest","/app-icon-512-512.493eca5b.png","/index.e307d365.js","/index.cf40d973.css"],n="bbf274c5";const t=`app-${n}`,a=[...e,"/"];self.addEventListener("install",(function(e){e.waitUntil(caches.open(t).then((function(e){return e.addAll(a)})))})),self.addEventListener("activate",(function(e){e.waitUntil(caches.keys().then((function(e){return Promise.all(e.filter((function(e){})).map((function(e){return caches.delete(e)})))})))})),self.addEventListener("fetch",(function(e){e.respondWith(caches.match(e.request).then((function(n){return n||fetch(e.request)})))}))})();
+//# sourceMappingURL=sw.js.map
